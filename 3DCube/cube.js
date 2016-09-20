d92ab1f20651cbb002f76bf8a79d7cfd9f2eaa4d
Alec Roth <Alexander.roth@drake.edu>
2016-09-19 19:35:54 -0500

    Finalized Assignment
    translation, scale, position and buttons all work.

---
+++ 3DCube/cube.js
@@ -0,0 +1,171 @@
+//Note that you do not need to alter this file for Assignment #4!!
+
+"use strict";
+
+var canvas;
+var gl;
+
+var NumVertices  = 36;
+
+var points = [];
+var colors = [];
+
+var xAxis = 0;
+var yAxis = 1;
+var zAxis = 2;
+
+var axis = 0;
+var theta = [ 0, 15, 0 ];  //rotation angles
+var trans = [0, 0, 0];     //translation values
+var scale = [1, 1, 1];     //scale values
+
+var thetaLoc;   //bridges to vectors in shaders
+var transLoc;
+var scaleLoc;
+
+window.onload = function init()
+{
+    canvas = document.getElementById( "gl-canvas" );
+
+    gl = WebGLUtils.setupWebGL( canvas );
+    if ( !gl ) { alert( "WebGL isn't available" ); }
+
+    colorCube();
+
+    gl.viewport( 0, 0, canvas.width, canvas.height );
+    gl.clearColor( 0.7, 0.7, 0.7, 1.0 );
+
+    gl.enable(gl.DEPTH_TEST);
+
+    //
+    //  Load shaders and initialize attribute buffers
+    //
+    var program = initShaders( gl, "vertex-shader", "fragment-shader" );
+    gl.useProgram( program );
+
+    var cBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, cBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(colors), gl.STATIC_DRAW );
+
+    var vColor = gl.getAttribLocation( program, "vColor" );
+    gl.vertexAttribPointer( vColor, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vColor );
+
+    var vBuffer = gl.createBuffer();
+    gl.bindBuffer( gl.ARRAY_BUFFER, vBuffer );
+    gl.bufferData( gl.ARRAY_BUFFER, flatten(points), gl.STATIC_DRAW );
+
+
+    var vPosition = gl.getAttribLocation( program, "vPosition" );
+    gl.vertexAttribPointer( vPosition, 4, gl.FLOAT, false, 0, 0 );
+    gl.enableVertexAttribArray( vPosition );
+
+    thetaLoc = gl.getUniformLocation(program, "theta");
+    transLoc = gl.getUniformLocation(program, "trans");
+    scaleLoc = gl.getUniformLocation(program, "scale");
+
+    //event listeners for buttons
+    //adjust the theta values so that cube rotations look neat
+    document.getElementById( "xButton" ).onclick = function () {
+        axis = xAxis;
+        theta[xAxis] = 0;
+        theta[yAxis] = 15;
+        theta[zAxis] = 0;
+    };
+    document.getElementById( "yButton" ).onclick = function () {
+        axis = yAxis;
+        theta[xAxis] = 45;
+        theta[yAxis] = 0;
+        theta[zAxis] = 0;
+    };
+    document.getElementById( "zButton" ).onclick = function () {
+        axis = zAxis;
+        theta[xAxis] = 45;
+        theta[yAxis] = 45;
+        theta[zAxis] = 0;
+    };
+
+    document.getElementById("xslider").onchange = function(event) {
+        trans[xAxis] = parseFloat(event.target.value);
+    };
+
+    document.getElementById("yslider").onchange = function(event) {
+        trans[yAxis] = parseFloat(event.target.value);
+    };
+
+    document.getElementById("xScaleSlider").onchange = function(event) {
+        scale[xAxis] = parseFloat(event.target.value);
+    };
+
+    document.getElementById("yScaleSlider").onchange = function(event) {
+        scale[yAxis] = parseFloat(event.target.value);
+    };
+
+    render();
+}
+
+function colorCube()
+{
+    quad( 1, 0, 3, 2 );
+    quad( 2, 3, 7, 6 );
+    quad( 3, 0, 4, 7 );
+    quad( 6, 5, 1, 2 );
+    quad( 4, 5, 6, 7 );
+    quad( 5, 4, 0, 1 );
+}
+
+function quad(a, b, c, d)
+{
+    var vertices = [
+        vec4( -0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5,  0.5,  0.5, 1.0 ),
+        vec4(  0.5, -0.5,  0.5, 1.0 ),
+        vec4( -0.5, -0.5, -0.5, 1.0 ),
+        vec4( -0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5,  0.5, -0.5, 1.0 ),
+        vec4(  0.5, -0.5, -0.5, 1.0 )
+    ];
+
+    var vertexColors = [
+        [ 0.0, 0.0, 0.0, 1.0 ],  // black
+        [ 1.0, 0.0, 0.0, 1.0 ],  // red
+        [ 1.0, 1.0, 0.0, 1.0 ],  // yellow
+        [ 0.0, 1.0, 0.0, 1.0 ],  // green
+        [ 0.0, 0.0, 1.0, 1.0 ],  // blue
+        [ 1.0, 0.0, 1.0, 1.0 ],  // magenta
+        [ 0.0, 1.0, 1.0, 1.0 ],  // cyan
+        [ 1.0, 1.0, 1.0, 1.0 ]   // white
+    ];
+
+    // We need to parition the quad into two triangles in order for
+    // WebGL to be able to render it.  In this case, we create two
+    // triangles from the quad indices
+
+    //vertex color assigned by the index of the vertex
+
+    var indices = [ a, b, c, a, c, d ];
+
+    for ( var i = 0; i < indices.length; ++i ) {
+        points.push( vertices[indices[i]] );
+        //colors.push( vertexColors[indices[i]] );
+
+        // for solid colored faces use
+        colors.push(vertexColors[a]);
+
+    }
+}
+
+function render()
+{
+    gl.clear( gl.COLOR_BUFFER_BIT | gl.DEPTH_BUFFER_BIT);
+
+    theta[axis] += 2.0;
+    gl.uniform3fv(thetaLoc, theta);
+    gl.uniform3fv(transLoc, trans);
+    gl.uniform3fv(scaleLoc, scale);
+
+    gl.drawArrays( gl.TRIANGLES, 0, NumVertices );
+
+    requestAnimFrame( render );
+}
